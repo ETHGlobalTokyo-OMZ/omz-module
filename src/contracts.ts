b1541ccfa38899c568c31b68197802f6ae14b6a7
tlatkdgus1 <tlatkdgus1@gmail.com>
2023-04-15 04:06:38 +0900

    update package version

--- src/contracts.ts
+++ src/contracts.ts
@@ -16,15 +16,27 @@ const contracts: Map<ChainIDEnums, IContract[]> = new Map([
             decimal: 18
         },
         {
+            type: ContractType.USDC,
+            tokenName: "USDC",
+            address: "0x1B487e41a3D806378eFdBC6E14ca96E31Ee2D90c",
+            decimal: 18
+        },
+        {
             type: ContractType.BOB,
             tokenName: "BOB",
-            address: "0x8efb12f288CB3e4Eb9300aDd97D1456980108F4f",
+            address: "0xe14B7E2a2Ce7CDe22e36f4518F9747B1e374e3a1",
             decimal: 18
+        },
+        {
+            type: ContractType.SELLER_VAULT,
+            tokenName: null,
+            address: "0x46DE81eA95C2540cC38CBA4e45cf749Ac3a80a5f",
+            decimal: null
         },
         {
             type: ContractType.ORDER_FACTORY,
             tokenName: null,
-            address: "0xb2B7e5C289679c8ea2D4BEC87c94c0f80941C603",
+            address: "0x24F72B93748EB1752dEb9b01A9944aC3238f8C5C",
             decimal: null
         }
     ]],
